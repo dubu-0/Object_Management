203a4f67b2c6e7f48d5a2c2d77e2e635d2a8bc2a
dubu-0 <yudin01@mail.ru>
2022-04-08 13:59:10 +0300

    Добавил возможность спавнить только на поверхности зоны

--- Assets/Scripts/SpawnZone.cs
+++ Assets/Scripts/SpawnZone.cs
@@ -1,7 +1,11 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpawnZone : MonoBehaviour
 {
+	[SerializeField] private bool _surfaceOnly;
+	
 	// Spawn point relative to this transform
-	public Vector3 SpawnPoint => transform.TransformPoint(Random.insideUnitSphere);
+	public Vector3 SpawnPoint => transform.TransformPoint(_surfaceOnly ? Random.onUnitSphere : Random.insideUnitSphere);
 }
